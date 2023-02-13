b9f9fbf9b1b7e98995f85e800f9690500c2679a0
xianruiyang <1390062436@qq.com>
2023-02-13 15:30:26 +0800

    简化

--- streamlit_test/pages/streamlit_test - Copy.py
+++ streamlit_test/pages/streamlit_test - Copy.py
@@ -2,9 +2,4 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
-
-f = open("E:\\Desktob\\program\\python\\streamlit_test\\streamlit_test\\web.html",'r',encoding='UTF-8')
-content = f.read()
-f.close()
-html(content,width=1002,height=2000,scrolling = True)
-
+my = st.write("测试")
